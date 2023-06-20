7fa0a163715b333b296651931b7309e020f8a97b
Lidiane Monteiro <lidiane.monteiro@luizalabs.com>
2023-06-20 19:25:39 -0300

    exemplo para branch main

--- exemplo.py
+++ exemplo.py
@@ -1,10 +1,4 @@
 print('Olá!')
 
 print('Adoro programar em Python \n'*30)
-nome = 'Lidiane'
 
-print('outro texto para erro')
-print('Adoro programar em Python \n'*30)
-# avisar qual arquivo queremos enviar (add)
-# comentário sobre a modificação (commit)
-# envio para o repositório remoto (push)
